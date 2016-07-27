7416c277b806483403235ceb044318d62097391a
pedrofelipefroes <pedrofelipefroes@gmail.com>
2016-07-26 23:58:53 -0300

    Foundation init

---
+++ jsx/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class App extends React.Component {
+  render () {
+    return <div> PF </div>
+  }
+}
+
+class Header extends React.Component {
+  render () {
+    <header></header>
+  }
+}
+
+export default App
